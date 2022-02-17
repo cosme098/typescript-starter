465a3734f8e8a3afbc62e279bce9a269ad39bc80
cosme098 <cosmeforeve@gmail.com>
2022-02-17 09:38:08 -0300

    fist commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,21 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getAll(): string {
     return this.appService.getHello();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id): object {
+    return this.appService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() newDevice): void {
+    return this.appService.newDevice(newDevice);
+  }
+  @Delete(':id')
+  delete(@Param('id') id): void {
+    return this.appService.delete(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,13 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+  getOne(id: string): object {
+    return {};
+  }
+  newDevice(newDevice): void {
+    console.log(newDevice);
+  }
+  delete(id: string): void {
+    console.log(id);
+  }
 }
